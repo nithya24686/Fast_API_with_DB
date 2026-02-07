22e94ccaa2baa6c6b53f25827ea841d2713ac8fe
nithya <tmnithya9812gmail.com>
2026-02-07 12:24:58 +0530

    commit

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,19 +1,21 @@
-from fastapi import APIRouter 
-from fastapi import Depends
+from fastapi import APIRouter
 from sqlalchemy.orm import Session
-from models import User
-from schemas.user_schemas import UserSchema
-from repositories.user_repo import UserRepo
+from fastapi import Depends
 from db import get_db
+from models import User
+from repositories.user_repo import UserRepo
+from schemas.user_schemas import UserSchema
 router = APIRouter()
 
+
 @router.post("/signup")
-def signup(user:UserSchema,db: Session = Depends(get_db)):
+def signup(user: UserSchema, db: Session = Depends(get_db)):
     user_repo = UserRepo(db)
-    user_repo.add_user(user)
+    # Convert Pydantic schema to SQLAlchemy model
+    db_user = User(email=user.email, password=user.password)
+    user_repo.add_user(db_user)
     return {"message": "User signed up successfully"}
 
 @router.post("/login")
 def login():
     return {"message": "User logged in successfully"}
-    
